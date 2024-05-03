d3781d654e7a1981a7a51d59ea790162af34e54f
MoreschiFederico <moreschifede2005@gmail.com>
2024-05-03 11:15:59 +0200

    prova

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
     const { HOST, USERNAME, PASSWORD, DBNAME, PORT } = process.env;
     console.log();
     const connpool = mysql.createPool({
-        host: "172.26.48.1",
+        host: "10.20.2.175",
         user: "tps",
         password: "ttppss",
         database: "td"
---
+++ utente.js
@@ -0,0 +1,120 @@
+function endpoint(app, connpool) {
+
+    app.post("/api/utenti", (req, res) => {
+        var errors = []
+        /* controllo dati inseriti
+        if (!req.body.description) {
+            errors.push("No description specified");
+        }
+        if (req.body.status === "") {
+            errors.push("No status specified");
+        }
+        */
+        if (errors.length) {
+            res.status(400).json({ "error": errors.join(",") });
+            return;
+        }
+        var data = {
+            username: req.body.username,
+            psw: req.body.psw,
+        }
+
+        var sql = 'INSERT INTO utente (username, psw) VALUES ("studente","studente")'
+        var params = [data.username, data.psw]
+        connpool.query(sql, params, (error, results) => {
+            if (error) {
+                res.status(400).json({ "error": error.message })
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": data,
+                "id": this.insertID
+            })
+            console.log(results)
+        });
+
+    })
+
+
+
+    app.get("/api/utenti", (req, res, next) => {
+        var sql = "select * from utente"
+        var params = []
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+              res.status(400).json({"error":err.message});
+              return;
+            }
+            res.json({
+                "message":"success",
+                "data":rows
+            })
+          });
+    });
+
+
+    app.get("/api/utenti/:1", (req, res) => {
+        var sql = "select * from utente where idUtente = 1"
+        var params = [req.params.id]
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+              res.status(400).json({"error":err.message});
+              return;
+            }
+            res.json({
+                "message":"success",
+                "data":rows[0]
+            })
+          });
+    });
+
+
+    app.put("/api/utenti/:1", (req, res) => {
+        var data = {
+            username: req.body.username,
+            psw: req.body.username,
+        }
+        connpool.execute(
+            `UPDATE utente set 
+               username = COALESCE(1,username), 
+               psw = COALESCE(1,psw) 
+               WHERE idUtente = 1`,
+            [data.username, data.psw, req.params.id],
+            function (err, result) {
+                if (err){
+                    res.status(400).json({"error": err.message})
+                    return;
+                }
+                console.log(result )
+                res.json({
+                    message: "success",
+                    data: data,
+                    changes: result.affectedRows
+                })
+        });
+    })
+
+
+
+    app.delete("/api/utenti/:1", (req, res) => {
+        connpool.execute(
+            'DELETE FROM utente WHERE idUtente = 1',
+            [req.params.id],
+            function (err, result) {
+                if (err){
+                    res.status(400).json({"error": err.message})
+                    return;
+                }
+                res.json({"message":"deleted", changes: result.affectedRows})
+        });
+    })
+
+
+}
+
+
+
+
+
+module.exports = endpoint;
