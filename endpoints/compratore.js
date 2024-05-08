b21d80a811fe833729d8bd30a40b86d959d45823
Alessio2901 <a.murgila11@gmail.com>
2024-05-08 10:37:30 +0200

    add

---
+++ endpoints/compratore.js
@@ -0,0 +1,117 @@
+function endpoint(app, connpool) {
+
+    app.post("/api/compratore", (req, res) => {
+        var errors = []
+        /* controllo dati inseriti
+        if (!req.body.description) {
+            errors.push("No description specified");
+        }
+        if (req.body.status === "") {
+            errors.push("No status specified");
+        }
+        */
+        if (errors.length) {
+            res.status(400).json({ "error": errors.join(",") });
+            return;
+        }
+        var data = {
+            nome: req.body.nome,
+        }
+
+        var sql = 'INSERT INTO compratore  (nome) VALUES (?)'
+        var params = [data.nome]
+        connpool.query(sql, params, (error, results) => {
+            if (error) {
+                res.status(400).json({ "error": error.message })
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": data,
+                "id": this.insertID
+            })
+            console.log(results)
+        });
+
+    })
+
+
+
+    app.get("/api/compratore", (req, res, next) => {
+        var sql = "select * from compratore"
+        var params = []
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": rows
+            })
+        });
+    });
+
+
+    app.get("/api/compratore/:id", (req, res) => {
+        var sql = "select * from compratore where idCompratore = ?"
+        var params = [req.params.id]
+        connpool.query(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": rows[0]
+            })
+        });
+    });
+
+
+    app.put("/api/compratore/:id", (req, res) => {
+        var data = {
+            nome: req.body.nome
+        }
+        connpool.execute(
+            `UPDATE compratore set 
+               nome = COALESCE(?,nome),  
+               WHERE idCompratore= ?`,
+            [data.nome, req.params.id],
+            function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+                console.log(result)
+                res.json({
+                    message: "success",
+                    data: data,
+                    changes: result.affectedRows
+                })
+            });
+    })
+
+
+
+    app.delete("/api/compratore/:id", (req, res) => {
+        connpool.execute(
+            'DELETE FROM utente WHERE idCompratore = ?',
+            [req.params.id],
+            function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+                res.json({ "message": "deleted", changes: result.affectedRows })
+            });
+    })
+
+
+}
+
+
+
+
+
+module.exports = endpoint;
